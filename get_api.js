f2eebf6daa07443abda6bdf432ebb1acd845697e
nardosteve <jackcale50@gmail.com>
2023-05-23 15:34:22 +0300

    Added favicon + Netflix logo (Image copyrights to Netflix Inc)

--- get_api.js
+++ get_api.js
@@ -2,17 +2,21 @@ function showMovies(){
 
     let data = null;
 
-    //Creating object to make requests
+    //Creating [Object] to make requests
     let xhr = new XMLHttpRequest();
 
     //Include the relevant credentials with the AJAX request
     xhr.withCredentials = true;
 
+    //Send a GET request to the api server
+    xhr.open('GET', 'https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en');
+
     //Call back function with response
     xhr.onload = function() {
-        if (xhr.status === 200) {
+        if (this.status === 200) {
             console.log("Success");
 
+            //Catch any error, else display
             try{
                 //Save response to movies variable
                 let response = JSON.parse(this.responseText);
@@ -22,11 +26,8 @@ function showMovies(){
 
                 //Loop through the movies array
                 movies.forEach(movie => {
-                   //console.log(movie.jawSummary.title);
-                   //console.log(movie.jawSummary.releaseYear);
                    let movieTitle = document.createElement('div');
                    movieTitle.innerHTML = movie.jawSummary.title;
-
                    document.getElementById('feed').appendChild(movieTitle);
                  });
 
@@ -36,9 +37,6 @@ function showMovies(){
 
         }
     }
-
-    //Send a request to the server
-    xhr.open('GET', 'https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en');
 
     //To POST data like an HTML form, add an HTTP header
     xhr.setRequestHeader('X-RapidAPI-Key', '934d8b23afmsh288ffa2f68508c9p180fa0jsn2495ee03f456');
